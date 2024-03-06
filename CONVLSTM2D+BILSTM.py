9dcab336cb2852142d9389df2979a12d43405f63
bhargavp2004 <105142165+bhargavp2004@users.noreply.github.com>
2024-03-06 13:52:51 +0530

    first commit

---
+++ CONVLSTM2D+BILSTM.py
@@ -0,0 +1,134 @@
+import cv2
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_score, recall_score
+from keras.models import Sequential
+from keras.layers import *
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping
+import os
+import random
+import matplotlib.pyplot as plt
+from keras.utils import to_categorical
+
+# Set seed for reproducibility
+seed_constant = 7
+
+IMAGE_HEIGHT, IMAGE_WIDTH = 64, 64
+SEQUENCE_LENGTH = 20
+DATASET_DIR = r'D:\B.TECH\6TH SEMESTER\Machine Learning\Project\Suspicious Activity Detection\Suspicious_Activity_Detection_ProjectML\Data_Extracted\Datasets\Datasets\Peliculas2'
+CLASSES_LIST = ["Fight", "Normal", "Shooting"]
+
+def frames_extraction(video_path):
+    frames_list = []
+    video_reader = cv2.VideoCapture(video_path)
+    video_frames_count = int(video_reader.get(cv2.CAP_PROP_FRAME_COUNT))
+    skip_frames_window = max(int(video_frames_count/SEQUENCE_LENGTH), 1)
+
+    for frame_counter in range(SEQUENCE_LENGTH):
+        video_reader.set(cv2.CAP_PROP_POS_FRAMES, frame_counter * skip_frames_window)
+        success, frame = video_reader.read()
+
+        if not success:
+            break
+
+        resized_frame = cv2.resize(frame, (IMAGE_HEIGHT, IMAGE_WIDTH))
+        normalized_frame = resized_frame / 255
+        frames_list.append(normalized_frame)
+
+    video_reader.release()
+    return frames_list
+
+def create_dataset():
+    features = []
+    labels = []
+    video_files_paths = []
+
+    for class_index, class_name in enumerate(CLASSES_LIST):
+        print(f'Extracting Data of Class: {class_name}')
+        files_list = os.listdir(os.path.join(DATASET_DIR, class_name))
+
+        for file_name in files_list:
+            video_file_path = os.path.join(DATASET_DIR, class_name, file_name)
+            frames = frames_extraction(video_file_path)
+
+            if len(frames) == SEQUENCE_LENGTH:
+                features.append(frames)
+                labels.append(class_index)
+                video_files_paths.append(video_file_path)
+
+    features = np.asarray(features)
+    labels = np.array(labels)
+
+    return features, labels
+
+features, labels = create_dataset()
+print("Features shape:", features.shape)
+
+one_hot_encoded_labels = to_categorical(labels)
+features_train, features_test, labels_train, labels_test = train_test_split(features, one_hot_encoded_labels, test_size=0.20, random_state=10)
+
+def create_convlstm_model():
+    model = Sequential()
+
+    model.add(ConvLSTM2D(filters=4, kernel_size=(3, 3), activation='tanh', data_format="channels_last",
+                         recurrent_dropout=0.2, return_sequences=True, input_shape=(SEQUENCE_LENGTH,
+                                                                                      IMAGE_HEIGHT, IMAGE_WIDTH, 3)))
+    model.add(MaxPooling3D(pool_size=(1, 2, 2), padding='same', data_format='channels_last'))
+    model.add(TimeDistributed(Dropout(0.2)))
+    
+    model.add(ConvLSTM2D(filters=8, kernel_size=(3, 3), activation='tanh', data_format="channels_last",
+                         recurrent_dropout=0.2, return_sequences=True))
+    model.add(MaxPooling3D(pool_size=(1, 2, 2), padding='same', data_format='channels_last'))
+    model.add(TimeDistributed(Dropout(0.2)))
+    
+    model.add(ConvLSTM2D(filters=14, kernel_size=(3, 3), activation='tanh', data_format="channels_last",
+                         recurrent_dropout=0.2, return_sequences=True))
+    
+    model.add(MaxPooling3D(pool_size=(1, 2, 2), padding='same', data_format='channels_last'))
+    model.add(TimeDistributed(Dropout(0.2)))
+    
+    model.add(ConvLSTM2D(filters=16, kernel_size=(3, 3), activation='tanh', data_format="channels_last",
+                         recurrent_dropout=0.2, return_sequences=True))
+    
+    model.add(MaxPooling3D(pool_size=(1, 2, 2), padding='same', data_format='channels_last'))
+    model.add(TimeDistributed(Dropout(0.2)))
+    
+    model.add(TimeDistributed(Flatten()))
+    
+    model.add(Bidirectional(LSTM(32)))
+    model.add(Dense(len(CLASSES_LIST), activation="softmax"))
+     
+    model.summary()
+    return model
+
+model = create_convlstm_model()
+
+early_stopping_callback = EarlyStopping(monitor='val_accuracy', patience=10, mode='max', restore_best_weights=True)
+
+model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
+model_training_history = model.fit(x=features_train, y=labels_train, epochs=50, batch_size=4, shuffle=True, validation_split=0.20, callbacks=[early_stopping_callback])
+
+# Plot training and validation accuracy
+plt.plot(model_training_history.history['accuracy'], label='Training Accuracy')
+plt.plot(model_training_history.history['val_accuracy'], label='Validation Accuracy')
+plt.title('Training and Validation Accuracy for convlstm')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+# Plot training and validation loss
+plt.plot(model_training_history.history['loss'], label='Training Loss')
+plt.plot(model_training_history.history['val_loss'], label='Validation Loss')
+plt.title('Training and Validation Loss for convlstm')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+# Evaluate the model on the test set
+test_loss, test_accuracy = model.evaluate(features_test, labels_test)
+print(f'Test Loss: {test_loss:.4f}')
+print(f'Test Accuracy: {test_accuracy:.4f}')
+model.save("Suspicious_Human_Activity_Detection_CONVLSTM.keras")
